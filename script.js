de6f46bb4f27ace0cefdc5050c13664dd8e7d7d0
HomamZituni <homam93@msn.com>
2025-11-07 11:55:00 -0500

    task 3 complete

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ return totalCost;
 }
 
 console.log(calculateTotalCost(10, 2, 0.1));
+
+
+// Task 3
+function checkEligibility (age, isEmployed) {
+if (age > 18 && isEmployed) {
+return "You are eligible";
+} else if (age > 18 && !isEmployed){
+return "You are conditionally eligible";
+} else {
+return "You are not eligible";
+} }
+
+
+console.log(checkEligibility(20, true));  
+console.log(checkEligibility(20, false)); 
+console.log(checkEligibility(16, true));  
